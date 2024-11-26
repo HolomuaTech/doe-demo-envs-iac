e133f29bf0a0d0dc5bbf4434486d6a1da2be5460
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-11-26 11:30:28 -1000

    Move domain mapping logic to the external cloud run module

--- cloud-run.tf
+++ cloud-run.tf
@@ -21,38 +21,13 @@ module "cloud_run_services" {
   dns_name        = chomp(data.google_dns_managed_zone.public_zone.dns_name)  # Pass the domain name
   cname_subdomain = each.value.cname_subdomain
 
+  # Domain mapping information
+  domain_name     = each.value.domain_name
+  project_number  = var.project_number
+
   # Optionally pass secrets to specific apps (adjust as needed)
   secret_name     = try(each.value.secret_name, null)
   secret_key      = try(each.value.secret_key, null)
   env_variable_name = try(each.value.env_variable_name, null)
 }
 
-# Create Cloud Run domain mapping for each app
-resource "google_cloud_run_domain_mapping" "domain_mapping" {
-  for_each = var.app_config
-
-  name     = each.value.domain_name
-  location = var.region
-
-  metadata {
-    namespace = var.project_number
-    annotations = {
-      "run.googleapis.com/override-headers" = "X-Forwarded-Proto=https"
-    }
-  }
-
-  spec {
-    route_name       = each.key
-    certificate_mode = "AUTOMATIC"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].annotations,
-      metadata[0].namespace,
-      spec[0].force_override
-    ]
-  }
-
-  depends_on = [module.cloud_run_services]
-}
